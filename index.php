1a95db90892b93fd6a10ce2700f0d3aded2da00c
Akadid <david.buisson@gmail.com>
2023-07-04 16:29:00 +0200

    Maj CSS 2

--- index.php
+++ index.php
@@ -13,7 +13,6 @@ if(isset($_SESSION['lastErrMsg'])){
     <title>GéoQuiz</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
@@ -23,15 +22,9 @@ if(isset($_SESSION['lastErrMsg'])){
         <form action="actions/login.php" method="POST">
             <label for="username">Entre ton prénom :</label>
             <input type="text" name="username" class="form-control" placeholder="Prénom" aria-label="Entre ton prénom :" required>
-            <button type="submit">Play!</button>
+            <button type="submit" class="btn btn-primary mt-3">Play!</button>
         </form>
     </div>
-
-  <!--  <form action="actions/login.php" method="POST">
-        <label for="username">Enter your username :</label>
-        <input type="text" name="username" required>
-        <button type="submit">Play!</button> -->
-    </form>
 </body>
 
 </html>
