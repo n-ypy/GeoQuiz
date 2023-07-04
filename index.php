c58c81eafdab2528d6b2bae8401a20e06ae95043
ebusy9 <g404ebus@gmail.com>
2023-07-04 11:40:15 +0200

    ajout de la fonctionnalité login

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <form action="actions/login.php" method="POST">
+        <label for="username">Enter your username :</label>
+        <input type="text" name="username" required>
+        <button type="submit">Play!</button>
+    </form>
+</body>
+</html>
