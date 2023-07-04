1048c693643f31bf439094477fa65167fcaa1078
ebusy9 <g404ebus@gmail.com>
2023-07-04 14:49:31 +0200

    petite modif

--- index.php
+++ index.php
@@ -6,14 +6,14 @@ if(isset($_SESSION['lastErrMsg'])){
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Home</title>
 </head>
 <body>
-    <form action="actions/login.php" method="POST" hidden>
+    <form action="actions/login.php" method="POST">
         <label for="username">Enter your username :</label>
         <input type="text" name="username" required>
         <button type="submit">Play!</button>
