a95c203b3bbbf6afa7464a7f93d97b470b526092
Akadid <david.buisson@gmail.com>
2023-07-11 12:30:26 +0200

    Maj CSS 12

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ session_start();
 if(isset($_SESSION['lastErrMsg'])){
     echo $_SESSION['lastErrMsg'];
 }
+
+
 ?>
 
 <!DOCTYPE html>
@@ -11,8 +13,6 @@ if(isset($_SESSION['lastErrMsg'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>GéoQuiz</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
@@ -20,8 +20,8 @@ if(isset($_SESSION['lastErrMsg'])){
     <div class="container">
         <h1>Bienvenue sur <b>GéoQuiz !</b></h1>
         <form action="actions/login.php" method="POST">
-            <input type="text" name="username" class="form-control" placeholder="Prénom" aria-label="Entre ton prénom :" required>
-            <button type="submit" class="btn mt-3">Jouer!</button>
+            <input type="text" name="username" class="form-control" placeholder="Prénom" value="<?=isset($_SESSION['username']) ? $_SESSION['username'] : "" ?>" aria-label="Entre ton prénom :" required>
+            <button type="submit" class="btn">Jouer!</button>
         </form>
     </div>
 </body>
