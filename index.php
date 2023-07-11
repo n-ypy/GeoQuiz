d2449bd496d87f67d1e49060ba419db5f0a698fc
ebusy9 <g404ebus@gmail.com>
2023-07-11 16:49:20 +0200

    aucune idee

--- index.php
+++ index.php
@@ -23,22 +23,26 @@ if (isset($_SESSION['lastErrMsg'])) {
         if (isset($_SESSION['id'])) {
             echo <<<html
             <nav id="topnav" style = "justify-content: end;">
-            <input type="button" value="Se déconnecter" onclick="location.href='actions/disconnect.php'">
+                <input type="button" value="Se déconnecter" onclick="location.href='actions/disconnect.php'">
             </nav>
             <h1>Bienvenue sur <b>GéoQuiz,</b> {$_SESSION['username']} !</h1>
-            <button type="submit" class="btn mt-3" onclick="window.location.href='leaderboard.php';">Leaderboard</button>
-            <button type="submit" class="btn mt-3" onclick="window.location.href='quizz.php';">Quizz v1</button>
-            <button type="submit" class="btn mt-3" onclick="window.location.href='quizz2.php';">Quizz v2</button>
+            <div id="btn-list-index">
+            <div id="row">
+               <button type="submit" class="btn" onclick="window.location.href='leaderboard.php';">Leaderboard</button>
+               </div>
+               <div id="column">
+               <button type="submit" class="btn" onclick="window.location.href='quizz.php';">Quizz v1</button>
+               <button type="submit" class="btn" onclick="window.location.href='quizz2.php';">Quizz v2</button>
+               </div>
+            </div>
         html;
         } else {
             echo <<<html
-
             <h1>Bienvenue sur <b>GéoQuiz !</b></h1>
             <form action="actions/login.php" method="POST">
                 <input type="text" name="username" class="form-control" placeholder="Prénom" aria-label="Entre ton prénom :" required>
-                <button type="submit" class="btn mt-3">Jouer!</button>
+                <button type="submit" class="btn">Jouer!</button>
             </form>
-
     html;
         }
         ?>
