19869711bd598b05e379659f22a0cf5177c14c3b
enypy <g404ebus@gmail.com>
2024-10-28 22:59:27 +0100

    db connection updt

--- api/actions/pdo-connection.php
+++ api/actions/pdo-connection.php
@@ -1,3 +1,16 @@
 <?php
+$dbHostname = getenv("MYSQL_HOSTNAME");
+$dbName = getenv("MYSQL_DB_NAME");
+$dbUsername = getenv("MYSQL_USERNAME");
+$dbPwd = getenv("MYSQL_PWD");
 
-require __DIR__ . '/../../actions/pdo-connection.php';
+try {
+    $pdoQuizz = new PDO("mysql:host=$dbHostname;dbname=$dbName", "$dbUsername", "$dbPwd", [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+} catch (PDOException $exception) {
+    $_SESSION['lastErrMsg'] = $exception->getMessage();
+    header('Location: ../index.php?err=PDOconnection');
+    exit();
+}
